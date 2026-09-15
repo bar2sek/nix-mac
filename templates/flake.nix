70cff8586d49dd45f2ea6399013be5b6f89e6022
Ryan Bartusek <bar2sek@outlook.com>
2026-09-14 22:26:50 -0500

    feat(zsh): add declarative hook for local untracked secrets

--- templates/flake.nix
+++ templates/flake.nix
@@ -101,6 +101,9 @@
         enableAutosuggestions = true;
         enableSyntaxHighlighting = true;
         promptInit = ''
+          # Load uncommitted local secrets (API keys, private tokens)
+          [[ ! -f ~/.zshrc.local ]] || source ~/.zshrc.local
+
           if [ -x "/opt/homebrew/bin/brew" ]; then
             eval "$(/opt/homebrew/bin/brew shellenv)"
           fi
